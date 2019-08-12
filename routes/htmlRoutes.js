721f0a8a079629c36cf34df3478e301ffabfdee3
Dan Beccaria <beccdaniel@gmail.com>
2019-08-12 11:55:52 -0400

    ... working on sending data through site

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,37 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
+module.exports = (app) => {
+
+  app.post('/api/vehicles', (req, res) => {
+    db.Cars.create({
+      make: req.body.make,
+      model: req.body.model,
+      year: req.body.year,
+      price: req.body.price
+    }).then(data => {
+      // console.log(data)
+    }).catch(err => {
+      console.log(err)
+    })
+    console.log(req.body)
+  })
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index")
+  
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  // // Render 404 page for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
