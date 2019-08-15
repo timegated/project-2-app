2e4c802edc16938397dc44caae6da1d8efcd77db
Dan Beccaria <beccdaniel@gmail.com>
2019-08-15 16:32:50 -0400

    fine tuning code and setting passport up

--- models/user.js
+++ models/user.js
@@ -1,15 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-    const Users = sequelize.define('Users',  {
-       username: {
-            type: DataTypes.STRING,
-            unique: true,
-            
-        },
-        password: {
-            type: DataTypes.STRING,
-            
-        },
-    })
-   
-    return Users
-};
+  const Users = sequelize.define('Users', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  })
+  return Users
+}
